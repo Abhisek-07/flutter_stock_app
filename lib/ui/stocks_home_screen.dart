b9c0ec86e24bafe91981aa24c46653f50bbebcf4
Abhisek Dash <abhisekdash6931@gmail.com>
2025-02-23 01:04:55 +0530

    first commit

---
+++ lib/ui/stocks_home_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class StocksHomeScreen extends StatelessWidget {
+  const StocksHomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
